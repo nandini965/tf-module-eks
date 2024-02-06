004e1b90c57dfa083b8d6c856fa1a6a595ef478b
nandini965 <nandhini965@gmail.com>
2024-02-07 01:48:17 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_eks_node_group" "node-group" {
 data "external" "thumb" {
   program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.eks.identity.0.oidc.0.issuer]
 }
-resource "aws_iam_openid_connect_provider" "default" {
+resource "aws_iam_openid_connect_provider" "cluster" {
   url = aws_eks_cluster.eks.identity.0.oidc.0.issuer
   client_id_list = ["sts.amazonaws.com"]
   thumbprint_list = [data.external.thumb.result.thumbprint]
