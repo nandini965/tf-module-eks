690125b85135fb67366a215df3e0674637771f13
nandini965 <nandhini965@gmail.com>
2024-02-07 11:11:52 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -35,12 +35,12 @@ resource "aws_eks_node_group" "node-group" {
     aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly-attach,
   ]
 }
-#
-#data "external" "thumb" {
-#  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.eks.identity.0.oidc.0.issuer]
-#}
-#resource "aws_iam_openid_connect_provider" "cluster" {
-#  url = aws_eks_cluster.eks.identity.0.oidc.0.issuer
-#  client_id_list = ["sts.amazonaws.com"]
-#  thumbprint_list = [data.external.thumb.result.thumbprint]
-#}
+
+data "external" "thumb" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.eks.identity.0.oidc.0.issuer]
+}
+resource "aws_iam_openid_connect_provider" "cluster" {
+  url = aws_eks_cluster.eks.identity.0.oidc.0.issuer
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.external.thumb.result.thumbprint]
+}
